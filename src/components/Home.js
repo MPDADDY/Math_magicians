d3d2b5ae5a574a9a5a28d6073eaec0a9c8e82f9b
Anold Mwagharo <anoldmwawasi@gmail.com>
2023-09-27 23:57:15 +0300

    Add Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div className="home">
+      <h1>Welcome on board</h1>
+      <p>This is a React web App  for arithmetic and scientific calculations.</p>
+      <p>Navigate to calculator whenever you want to do some atithmetic calculations</p>
+    </div>
+  );
+}
+export default Home;
